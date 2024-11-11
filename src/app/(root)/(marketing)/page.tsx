ec69d453d659c245b7b8b5233e5df75cc4d36e96
Przemysław Galarowicz <pgalarowicz@gmail.com>
2024-11-11 13:55:20 +0100

    Add tmp home page

--- src/app/(root)/(marketing)/page.tsx
+++ src/app/(root)/(marketing)/page.tsx
@@ -1,3 +1,10 @@
+import { Logo } from '@/components/logo'
+
 export default async function HomePage() {
-  return <>Home</>
+  return (
+    <div className="flex h-screen flex-col items-center justify-center gap-10">
+      <Logo href="/" />
+      <span className="text-sm text-muted-foreground">Soon...</span>
+    </div>
+  )
 }
